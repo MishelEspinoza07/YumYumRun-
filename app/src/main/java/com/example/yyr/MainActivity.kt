e83c839643c3ea76e751c6f8f6c9c48662470259
Alexia Marin <alexiad.marinm@gmail.com>
2023-06-18 19:43:50 -0700

    calificanoss

--- app/src/main/java/com/example/yyr/MainActivity.kt
+++ app/src/main/java/com/example/yyr/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.yyr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yyr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.signup)
     }
 }
