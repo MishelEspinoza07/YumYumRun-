a8b6aca50df71e4956ad854479a6c0234177d63a
joey0411cr <jose411cortez@gmail.com>
2023-06-19 07:49:28 -0400

    login er0

--- app/src/main/java/com/example/yyr/MainActivity.kt
+++ app/src/main/java/com/example/yyr/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.example.yyr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.yyr.databinding.ActivityMainBinding
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+//import com.example.yyr.databinding.ActivityMainBinding
 
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    lateinit var signing: Button
+    private val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
          setTheme(R.style.Base_Theme_YYR)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+
+    }
+
+    fun initViews(){
+        signing = findViewById(R.id.bt_iniciar_sesion)
+        signing.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Log.d(TAG, "siguiente pantalla")
+        val intent = Intent(this, Login::class.java)
+        startActivity(intent)
         setContentView(R.layout.signup)
     }
 }
