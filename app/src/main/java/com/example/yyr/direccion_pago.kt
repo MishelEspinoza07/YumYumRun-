db9bd783559e1bb610897e8829aea0dea25aa567
MISHEL ESPINOZA <mishelespinoza0624@gmail.com>
2023-06-20 09:03:55 -0400

    Direccion de Pago

---
+++ app/src/main/java/com/example/yyr/direccion_pago.kt
@@ -0,0 +1,11 @@
+package com.example.yyr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class direccion_pago : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_direccion_pago)
+    }
+}
